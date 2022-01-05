9162239fd8266e586a820698d0fd62a535cddbcd
Jelger <jelgervitt@gmail.com>
2022-01-04 22:25:38 -0800

    Finished the etch-a-sketch exercise

--- script.js
+++ script.js
@@ -1,44 +1,51 @@
 // create a reference to the container div that will hold the squares
 const container = document.querySelector('.container');
 const button = document.querySelector('button');
-let gridCells;
+const containerSize = container.clientHeight;
+
 
 
 // on page load, main container fills with 16 rows, and 16 squares
-window.addEventListener('load', () => {
-    for (let i = 0 ; i < 16 ; i++) {
+window.addEventListener('load', drawSquares(16))
+
+function drawSquares(cellAmt) {
+    let cellSize = containerSize / cellAmt;
+    for (let i = 0 ; i < cellAmt ; i++) {
         let row = document.createElement('div');
-        row.class = 'row;';
+        row.classList = 'row';
         container.appendChild(row);   
-        for (let i = 0 ; i < 16 ; i++) {
+        for (let i = 0 ; i < cellAmt ; i++) {
             let square = document.createElement('div');
             square.classList = 'square';
+            square.style.height = cellSize + 'px';
+            square.style.width = cellSize + 'px';
             row.appendChild(square);
             etch(square);
         }
     }
-})
+};
 
 // on hover, cell changes to black (etching)
 function etch(square) {
     square.addEventListener ('mouseover', () => {
         square.classList.add('etched');
-        console.log("etched");
     })
 }
 
 // clear button clears the sketch, and asks for new grid size
 button.addEventListener('click', () => {
-    // clear the sketch
     const etchedCells = Array.from(document.querySelectorAll('.etched'));
-    etchedCells.forEach(cell => cell.classList.remove('etched'));
     // ask for size of grid
     let gridSize = prompt("Enter the new grid size (cells/side)");
-    if (gridSize !== null) {
-        // gridCells = 960 / 
-
-
+    // Cap the grid size at 100 cells
+    while (gridSize > 100) gridSize = prompt ("Please enter a max amt of 100 cells");
+    // if a correct number is entered, redraw the grid
+    if (gridSize !== null && gridSize <= 100) {
+        while (container.firstChild) container.removeChild(container.lastChild);
+        drawSquares(gridSize);
     }
+    // otherwise, just clear the grid
+    etchedCells.forEach(cell => cell.classList.remove('etched'));
 })
 
 // the container is 960px, build a function that sets 
